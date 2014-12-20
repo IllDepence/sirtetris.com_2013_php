a311cda0a4dc006cdd59b51a70f886b6729a8d89
tarek <tareksaier@gmail.com>
2014-12-20 16:11:52 +0100

    blog now sans comments but with tags

--- acp.php
+++ acp.php
@@ -14,68 +14,70 @@ $entries = json_decode($entries_json);
 usort($entries, 'cmp_obj_by_date_attr');
 
 if(strlen($_GET['edit'])) {
-	foreach($entries as $e) {
-		if($e->id == $_GET['edit']) {
-			$to_edit = $e;
-			break;
-			}
-		}
-	}
+    foreach($entries as $e) {
+        if($e->id == $_GET['edit']) {
+            $to_edit = $e;
+            break;
+            }
+        }
+    }
 
 echo '<form method="POST" action="acp.php">'.
-	'headline: <input type="text" name="headline" value="'.(strlen($_GET['edit']) ? htmlspecialchars($to_edit->headline) : '').'" /><br />'.
-	'picture: <input type="text" name="image" value="'.(strlen($_GET['edit']) ? $to_edit->image : '').'" /><br />'.
-	'text: <textarea name="text" style="width: 1000px; height: 400px;">'.(strlen($_GET['edit']) ? htmlspecialchars($to_edit->text) : '').'</textarea><br />'.
-	(strlen($_GET['edit']) ? '<input type="hidden" name="replace" value="'.$to_edit->id.'"/>' : '').
-	'<input type="submit" />'.
-	'</form><br />';
+    'headline: <input type="text" name="headline" value="'.(strlen($_GET['edit']) ? htmlspecialchars($to_edit->headline) : '').'" /><br />'.
+    'picture: <input type="text" name="image" value="'.(strlen($_GET['edit']) ? $to_edit->image : '').'" /><br />'.
+    'tags: <input type="text" name="tags" value="'.(strlen($_GET['edit']) ? implode(',', $to_edit->tags) : '').'" /><br />'.
+    'text: <textarea name="text" style="width: 1000px; height: 400px;">'.(strlen($_GET['edit']) ? htmlspecialchars($to_edit->text) : '').'</textarea><br />'.
+    (strlen($_GET['edit']) ? '<input type="hidden" name="replace" value="'.$to_edit->id.'"/>' : '').
+    '<input type="submit" />'.
+    '</form><br />';
 
 if(strlen($_POST['headline'])>0 && strlen($_POST['text'])>0) {
-	$msg = '&gt;&gt; enrty ';
-	$new_entry = new stdClass();
-	$new_entry->text = $_POST['text'];
-	if(isset($_POST['replace']) && strlen($_POST['replace'])>0) {
-		foreach($entries as $i => $e) {
-			if($e->id == $_POST['replace']) {
-				array_splice($entries, $i, 1);
-				$new_entry->id = $e->id;
-				$new_entry->date = $e->date;
-				$msg .= 'edited';
-				break;
-				}
-			}
-		}
-	else {
-		$new_entry->id = hash('adler32', $new_entry->text, false);
-		$new_entry->date = date('Y-m-d');
-		$msg .= 'added';
-		}
-	$new_entry->headline = $_POST['headline'];
-	$new_entry->image = $_POST['image'];
-	array_push($entries, $new_entry);
+    $msg = '&gt;&gt; enrty ';
+    $new_entry = new stdClass();
+    $new_entry->text = $_POST['text'];
+    $new_entry->tags = explode(',', $_POST['tags']);
+    if(isset($_POST['replace']) && strlen($_POST['replace'])>0) {
+        foreach($entries as $i => $e) {
+            if($e->id == $_POST['replace']) {
+                array_splice($entries, $i, 1);
+                $new_entry->id = $e->id;
+                $new_entry->date = $e->date;
+                $msg .= 'edited';
+                break;
+                }
+            }
+        }
+    else {
+        $new_entry->id = hash('adler32', $new_entry->text, false);
+        $new_entry->date = date('Y-m-d');
+        $msg .= 'added';
+        }
+    $new_entry->headline = $_POST['headline'];
+    $new_entry->image = $_POST['image'];
+    array_push($entries, $new_entry);
 
-	$entries_json = json_encode($entries);
-	$fh = fopen('src/blog/entries.json', 'w');
-	fwrite($fh, $entries_json);
-	fclose($fh);
-	echo $msg;
-	}
+    $entries_json = json_encode($entries);
+    $fh = fopen('src/blog/entries.json', 'w');
+    fwrite($fh, $entries_json);
+    fclose($fh);
+    echo $msg;
+    }
 else if(isset($_GET['del'])) {
-	foreach($entries as $i => $e) {
-		if($e->id == $_GET['del']) {
-			array_splice($entries, $i, 1);
-			break;
-			}
-		}
-	$entries_json = json_encode($entries);
-	$fh = fopen('src/blog/entries.json', 'w');
-	fwrite($fh, $entries_json);
-	fclose($fh);
-	echo '&gt;&gt; enrty deleted.';
-	}
+    foreach($entries as $i => $e) {
+        if($e->id == $_GET['del']) {
+            array_splice($entries, $i, 1);
+            break;
+            }
+        }
+    $entries_json = json_encode($entries);
+    $fh = fopen('src/blog/entries.json', 'w');
+    fwrite($fh, $entries_json);
+    fclose($fh);
+    echo '&gt;&gt; enrty deleted.';
+    }
 else {
-	echo '&gt;&gt; fill in the form.';
-	}
+    echo '&gt;&gt; fill in the form.';
+    }
 
 echo '<br /><br />';
 
--- functions.php
+++ functions.php
@@ -1,102 +1,64 @@
 <?php
 
 function block_shitheads() {
-	$agent_text = 'Dear visitor, the user agent string your browser sent this server contains an HTML &lt;a&gt; tag. This is a common practice to spam logs with bullshit links. Therefore your access to this page was revoked. If you\'re a normal human visitor not using some funny manipulated user agent string PLEASE try to access this page with another browser to get my contact information and inform me about this error.';
-	$ref_text = 'Dear visitor, your the browser appears to have sent this server a fake HTTP referrer. This is a common practice to spam logs with bullshit links. Therefore your access to this page was revoked. If you\'re a normal human visitor not using some funny manipulated user agent string PLEASE try to access this page with another browser to get my contact information and inform me about this error.';
+    $agent_text = 'Dear visitor, the user agent string your browser sent this server contains an HTML &lt;a&gt; tag. This is a common practice to spam logs with bullshit links. Therefore your access to this page was revoked. If you\'re a normal human visitor not using some funny manipulated user agent string PLEASE try to access this page with another browser to get my contact information and inform me about this error.';
+    $ref_text = 'Dear visitor, your the browser appears to have sent this server a fake HTTP referrer. This is a common practice to spam logs with bullshit links. Therefore your access to this page was revoked. If you\'re a normal human visitor not using some funny manipulated user agent string PLEASE try to access this page with another browser to get my contact information and inform me about this error.';
 
-	if(preg_match('/<a\s+href="/i', $_SERVER['HTTP_USER_AGENT'])) die($agent_text);
-	if(preg_match('/http:\/\/hand-made-soaps\.com/i', $_SERVER['HTTP_REFERER'])) die($ref_text);
-	if(preg_match('/http:\/\/pony-business\.com/i', $_SERVER['HTTP_REFERER'])) die($ref_text);
-	}
+    if(preg_match('/<a\s+href="/i', $_SERVER['HTTP_USER_AGENT'])) die($agent_text);
+    if(preg_match('/http:\/\/hand-made-soaps\.com/i', $_SERVER['HTTP_REFERER'])) die($ref_text);
+    if(preg_match('/http:\/\/pony-business\.com/i', $_SERVER['HTTP_REFERER'])) die($ref_text);
+    }
 
 function cmp_obj_by_date_attr($a, $b) {
-	$a = intval(str_replace(Array('-',' ',':'), '', $a->date));
-	$b = intval(str_replace(Array('-',' ',':'), '', $b->date));
-	if ($a == $b) return 0;
-	return (($a > $b) ? -1 : 1);
-	}
+    $a = intval(str_replace(Array('-',' ',':'), '', $a->date));
+    $b = intval(str_replace(Array('-',' ',':'), '', $b->date));
+    if ($a == $b) return 0;
+    return (($a > $b) ? -1 : 1);
+    }
 
 function urlsave($s) {
-	return preg_replace('/\W/', '_', $s);
-	}
+    return preg_replace('/\W/', '_', $s);
+    }
 
 function youtubedd($label, $id) {
-	return 	"<span id=\"nojs_wrapper_".$id."\" style=\"display: inline;\"><p><a href=\"http://youtu.be/".$id."\">".$label."</a></p></span>".
-		"<span id=\"js_wrapper_".$id."\" style=\"display: none;\"><p><span id=\"trigger_".$id."\">".
-			"<span class=\"a\" onClick=\"dropdown('".$id."', '<iframe width=\'700\' height=\'436\' src=\'http://www.youtube.com/embed/".$id."\' frameborder=\'0\' allowfullscreen></iframe>', 'open', '".$label."')\"".
-			" onMouseOver=\"this.style.cursor='pointer'\">".$label."</span>".
-		"</span></p>".
-		"<div id=\"cmnt_".$id."\"></div></span>".
-		"<script type=\"text/javascript\">document.getElementById(\"nojs_wrapper_".$id."\").setAttribute(\"style\", \"display:none;\");".
-		"document.getElementById(\"js_wrapper_".$id."\").setAttribute(\"style\", \"display:inline;\")</script>";
-	}
+    return  "<span id=\"nojs_wrapper_".$id."\" style=\"display: inline;\"><p><a href=\"http://youtu.be/".$id."\">".$label."</a></p></span>".
+        "<span id=\"js_wrapper_".$id."\" style=\"display: none;\"><p><span id=\"trigger_".$id."\">".
+            "<span class=\"a\" onClick=\"dropdown('".$id."', '<iframe width=\'700\' height=\'436\' src=\'http://www.youtube.com/embed/".$id."\' frameborder=\'0\' allowfullscreen></iframe>', 'open', '".$label."')\"".
+            " onMouseOver=\"this.style.cursor='pointer'\">".$label."</span>".
+        "</span></p>".
+        "<div id=\"cmnt_".$id."\"></div></span>".
+        "<script type=\"text/javascript\">document.getElementById(\"nojs_wrapper_".$id."\").setAttribute(\"style\", \"display:none;\");".
+        "document.getElementById(\"js_wrapper_".$id."\").setAttribute(\"style\", \"display:inline;\")</script>";
+    }
 
 function youtube($id) {
-	return "<iframe width=\"700\" height=\"436\" src=\"http://www.youtube.com/embed/".$id."\" frameborder=\"0\" allowfullscreen></iframe>";
-	}
-
-function getimages($moreinfo=0, $givendir=0) {
-	if($givendir) {
-		$directory = $givendir;
-		}
-	else {
-		if($_GET['c'] == "photography") { $add = "/fullsize"; }
-		else { $add = ""; }
-		$directory = "img/".$_GET['c'].$add;
-		}
-
-	$dir=opendir($directory);
-	$x=0;
-
-	while ($file = readdir ($dir)) {
-		if ($file != "." && $file != "..") {
-			$image[$x]=$file;
-			if($moreinfo) {
-				$isize=getimagesize($directory."/".$file);
-				$iwidth[$file] = $isize[0];
-				$iheight[$file] = $isize[1];
-				$fsize[$file]=filesize($directory."/".$file);
-			}
-			$x++;
-		}
-	}
-
-	sort($image);
-
-	closedir($dir);
-	return ($moreinfo ? array($image, $iwidth, $iheight, $fsize) : $image);
-	}
-
-function fixname($title) {
-	$markup = array('-ae-', '-oe-', '-ue-', '-Ae-', '-Oe-', '-Ue-', '-ss-', '-fz-', '-az-', '-mdash-', '_', '-strn-', '-dots-', '-slash-');
-	$replacement =	array('&auml;', '&ouml;', '&uuml;', '&Auml;', '&Ouml;', '&Uuml;', '&szlig', '?', '!', '&mdash;', ' ', '*', '...', '/');
-	return str_replace($markup, $replacement, $title);
-	}
+    return "<iframe width=\"700\" height=\"436\" src=\"http://www.youtube.com/embed/".$id."\" frameborder=\"0\" allowfullscreen></iframe>";
+    }
 
 function log_visit() {
-	if ($_COOKIE['visits'] && is_numeric($_COOKIE['visits'])) {
-		$hits = $_COOKIE['visits'];
-		}
-	else {
-		$hits = 0;
-	}
+    if ($_COOKIE['visits'] && is_numeric($_COOKIE['visits'])) {
+        $hits = $_COOKIE['visits'];
+        }
+    else {
+        $hits = 0;
+    }
 
-	$hits++;
+    $hits++;
 
-	setcookie('visits', $hits, time()+60*60*24*365);
+    setcookie('visits', $hits, time()+60*60*24*365);
 
-	$new_log_entry = new stdClass();
-	$new_log_entry->timestamp = time();
-	$new_log_entry->hits = $hits;
-	$new_log_entry->http_referer = $_SERVER['HTTP_REFERER'];
-	$new_log_entry->query_string = $_SERVER['QUERY_STRING'];
-	$new_log_entry->remote_addr = $_SERVER['REMOTE_ADDR'];
-	$new_log_entry->user_agent = $_SERVER['HTTP_USER_AGENT'];
+    $new_log_entry = new stdClass();
+    $new_log_entry->timestamp = time();
+    $new_log_entry->hits = $hits;
+    $new_log_entry->http_referer = $_SERVER['HTTP_REFERER'];
+    $new_log_entry->query_string = $_SERVER['QUERY_STRING'];
+    $new_log_entry->remote_addr = $_SERVER['REMOTE_ADDR'];
+    $new_log_entry->user_agent = $_SERVER['HTTP_USER_AGENT'];
 
-	$entry_json = json_encode($new_log_entry)."\n";
-	$fh = fopen('src/logs/logs.json', 'a');
-	fwrite($fh, $entry_json);
-	fclose($fh);
-	}
+    $entry_json = json_encode($new_log_entry)."\n";
+    $fh = fopen('src/logs/logs.json', 'a');
+    fwrite($fh, $entry_json);
+    fclose($fh);
+    }
 
 ?>
